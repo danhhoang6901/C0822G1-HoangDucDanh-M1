77e58f95f05ee015e7b6969f5bd7860917d2f309
danhhoang6901 <danhhoang0196@gmail.com>
2023-04-11 09:29:01 +0700

    DanhHD udate

---
+++ module1/ss7_bien_kieu_du_lieu_toan_tu/thuc_hanh/toan_tu.js
@@ -0,0 +1,6 @@
+let inputwidth=prompt("Nhập vào chiều rộng");
+let inputheight=prompt("Nhập vào chiều cao");
+let width=parseInt(inputwidth);
+let height=parseInt(inputheight);
+let area=width*height;
+document.write("Diện tích hcn là: "+area);
